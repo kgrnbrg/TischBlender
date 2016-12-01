2c4b213e7862bdbe2bdddf50480dca9365edad2c
Kyle Greenberg <kgrnbrg@gmail.com>
2016-12-01 15:44:53 -0500

    file mgmt

--- scriptRSS.js
+++ scriptRSS.js
@@ -2,36 +2,30 @@
 var r,g,b;
 var projs = [];
 var cv;
-
+var json;
 function setup(){
     cv = createCanvas(windowWidth,windowHeight);
     r = random(255);
     g = random(255);
     b = random(255);
     background(r,g,b);
+    json = {};
 }   
 
 function Jitter(x,y){
     this.x = random(width);
     this.y = random(height);
     this.diameter = random(50,100);
-//    this.speed = 5;
     
-//    this.move = function(){
-//       this.x += random(-this.speed, this.speed);
-//        this.y += random(-this.speed, this.speed);
-//        
-//       
-//  },
     this.display = function(r,g,b) {
         stroke(.5);
         fill(r,g,b);
-        ellipse(this.x, this.y, this.diameter,this.diameter);
+        rect(this.x, this.y, this.diameter,this.diameter);
         
     },
     this.clicked = function(){
        var d = dist(mouseX, mouseY, this.x, this.y);
-       if (d < 25 ) {
+       if (d < 40 ) {
         return true;
        this.color = color(255,0,200);   
      }
@@ -40,7 +34,7 @@ function Jitter(x,y){
    },
     this.changeColor = function(r,g,b){
         this.display(r,g,b);
-        console.log("one circle")
+        
     }
 }
 
@@ -55,21 +49,25 @@ var something;
 $.get(call, function( data ) {
     senback(data);
     vizViz(data);
-    //mousePressed(data);
-     
-for (i=0; i < data.length; i++){
-        var k = data[i].link;
-        console.log("data:" + data[i]);  
-        console.log(k);
-    }  
+    console.log(data);
+
 });
+    
+// for (i=0; i < data.length; i++){
+//   div = data[i].link + ;
+     
+ //}
+//        var k = data[i].link;
+//        console.log("data:" + data[i]);  
+//        console.log(k);
+ // }  
+
 
 function senback(data){
     something = data;
-    console.log(data);
       for (var i=0; i<something.length; i++) {
         projs.push(new Jitter()); 
-          
+        
   }
     
 }
@@ -80,9 +78,9 @@ function vizViz(data){
         var g = random(255);
         var b = random(255);
         projs[i].display(r,g,b);
-   
-         }
-       }
+       
+         }    
+}
 
 function mousePressed(){
     console.log(projs.length)
@@ -93,14 +91,12 @@ function mousePressed(){
                 var g = random(255);
                 var b = random(255);
                 projs[i].changeColor(r,g,b);
+                
             }
         }
     }
 function draw(){
-//    for(var i in projs){
-//     projs[i].move();
-     //projs[i].display();
-    //}
+
 }
 
 
